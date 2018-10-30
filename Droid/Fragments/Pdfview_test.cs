a1c2a08cb94538de1340486ca356613a57782b55
baselabubaker <basel.abubaker@hotmail.com>
2018-10-30 15:53:25 +0400

    PDF progress

--- Droid/Fragments/Pdfview_test.cs
+++ Droid/Fragments/Pdfview_test.cs
@@ -15,6 +15,7 @@ using Android.Views;
 using Android.Widget;
 using Com.Joanzapata.Pdfview;
 using Dmax.Dialog;
+using NPCCMobileApplications.Library;
 
 namespace NPCCMobileApplications.Droid
 {
@@ -23,7 +24,10 @@ namespace NPCCMobileApplications.Droid
         LayoutInflater InflaterMain;
         View view;
         PDFView pdfView;
-        AlertDialog dialog;
+        ProgressBar progress_bar;
+        RelativeLayout ProgressCont;
+        TextView proTextView;
+        string pdfLink;
 
         DocBroadcastReceiver receiver;
         private static Pdfview_test ins;
@@ -42,6 +46,9 @@ namespace NPCCMobileApplications.Droid
 
         public void ShowPDFDoc(String path)
         {
+            //this.Activity.UnregisterReceiver(receiver);
+            ProgressCont.Visibility = ViewStates.Gone;
+
             ins.Activity.RunOnUiThread(() => {
                 Java.IO.File filePath = new Java.IO.File(path);
                 // Check whether the file is exist in the download directory 
@@ -55,51 +62,49 @@ namespace NPCCMobileApplications.Droid
                     throw new FileNotFoundException("File not found" + filePath.AbsolutePath.ToString());
                 }
             });
-            dialog.Dismiss();
+        }
+
+        public void ProgressBar(long totalSize, long BytesReceived)
+        {
+
+            ins.Activity.RunOnUiThread(() => {
+                if(progress_bar.Max == 100)
+                progress_bar.Max = (int)totalSize;
+                progress_bar.Progress =(int)BytesReceived;
+                proTextView.Text = npcc_services.npcc_SizeSuffix(BytesReceived) + "/" + npcc_services.npcc_SizeSuffix(totalSize);
+            });
         }
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             InflaterMain = inflater;
             view = inflater.Inflate(Resource.Layout.Pdfview_test, container, false);
+
+
             pdfView = view.FindViewById<PDFView>(Resource.Id.pdfview);
 
-            Intent downloadIntent = new Intent(this.Activity, typeof(LongRunningThread));
+            progress_bar = view.FindViewById<ProgressBar>(Resource.Id.progress_bar);
 
-            downloadIntent.PutExtra("file_to_download", "https://edms.npcc.ae/NDMS/PublicDocuPreview.aspx?ORIG=P&ID-SFNY-STP-18-VER-00001.pdf&DocuVersID=5057633&SID=O");
+            ProgressCont = view.FindViewById<RelativeLayout>(Resource.Id.ProgressCont);
 
-            dialog = new SpotsDialog(ins.Context, Resource.Style.CustomDialog);
-            dialog.SetMessage("Downloading PDF...");
-            dialog.SetCancelable(false);
-            dialog.Show();
+            proTextView = view.FindViewById<TextView>(Resource.Id.proTextView);
 
+            pdfLink = "https://edms.npcc.ae/NDMS/PublicDocuPreview.aspx?ORIG=P&ID-SFNY-STP-18-VER-00001.pdf&DocuVersID=5057633&SID=O";
+            //pdfLink = "http://todobanderas.com/documents/completeflagsoftheworld.pdf";
+        
+            Intent downloadIntent = new Intent(this.Activity, typeof(PDFLongRunningThread));
+            downloadIntent.PutExtra("file_to_download", pdfLink);
+        
+            this.Activity.RegisterReceiver(receiver, new IntentFilter("PDFDownloading"));
             this.Activity.StartService(downloadIntent);
 
             return view;
         }
 
-        public override void OnResume()
+        public override void OnDestroy()
         {
-            base.OnResume();
-            this.Activity.RegisterReceiver(receiver, new IntentFilter("PDFDownloading"));
-        }
-
-        public override void OnPause()
-        {
-            base.OnPause();
+            base.OnDestroy();
             this.Activity.UnregisterReceiver(receiver);
-        }
-
-    }
-
-    [BroadcastReceiver(Enabled = true)]
-    [IntentFilter(new[] { "PDFDownloading" })]
-    public class DocBroadcastReceiver : BroadcastReceiver
-    {
-        public override void OnReceive(Context context, Intent intent)
-        {
-            string file_to_view = intent.GetStringExtra("file_to_view");
-            Pdfview_test.getInstace().ShowPDFDoc(file_to_view);
         }
     }
 }
---
+++ Droid/class/PDFLongRunningThread.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+
+namespace NPCCMobileApplications.Droid
+{
+    [Service]
+    public class PDFLongRunningThread : Service
+    {
+
+        volatile bool isRunning;
+        string documentPath;
+        Uri uri;
+        WebClient client;
+
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+        public override void OnDestroy()
+        {
+            isRunning = false;
+            client.Dispose();
+            base.OnDestroy();
+        }
+
+        public override StartCommandResult OnStartCommand(Intent intent, [GeneratedEnum] StartCommandFlags flags, int startId)
+        {
+            isRunning = true;
+            Task.Run(() =>
+            {
+                if (isRunning == true)
+                {
+                    string fileToDownload = intent.GetStringExtra("file_to_download");
+                    SavePdf("pdfDoc.pdf", fileToDownload);
+                }
+                if (isRunning == true) 
+                    this.StopSelf();
+                    
+            });
+            return StartCommandResult.Sticky;
+        }
+
+        private void SavePdf(string filename, string link)
+        {
+            documentPath = Path.Combine((Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDownloads)).Path, filename);
+
+            client = new WebClient();
+            uri = new Uri(link);
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            client.DownloadProgressChanged += Client_DownloadProgressChanged;
+            client.DownloadFileAsync(uri, documentPath);
+        }
+
+        void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            WebClient wc = (WebClient)sender;
+            long totalSize = long.Parse(wc.ResponseHeaders["Content-Length"]);
+            if(Pdfview_test.getInstace().Activity == null){
+                client.CancelAsync();
+                return;
+            }
+            Pdfview_test.getInstace().ProgressBar(totalSize, e.BytesReceived);
+            if(e.TotalBytesToReceive == e.BytesReceived)
+            {
+                Intent message = new Intent("PDFDownloading");
+                message.PutExtra("file_to_view", documentPath);
+                SendBroadcast(message);
+            }
+
+        }
+
+
+    }
+
+    [BroadcastReceiver(Enabled = true)]
+    [IntentFilter(new[] { "PDFDownloading" })]
+    public class DocBroadcastReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            string file_to_view = intent.GetStringExtra("file_to_view");
+            Pdfview_test.getInstace().ShowPDFDoc(file_to_view);
+        }
+    }
+}
